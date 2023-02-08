f00e3483f944cb57187c9ad4b0a3d9d59c2c5a24
Lawrence137 <lawrencekimani18@gmail.com>
2023-02-08 08:33:02 +0300

    complete

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+    while seconds > 0 do
+        puts "#{seconds} SECOND(S)!"
+        seconds -= 1
+    end
+    'HAPPY NEW YEAR!'
 end
+countdown(10)
+
+
+def countdown_with_sleep(seconds)
+    (seconds).downto(1) do |i|
+      sleep 1
+      puts i
+    end
+  end
